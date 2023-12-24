27b12d4b4c12fc4259e49179870997ea7bd6aa52
arttturslv <arttturslv@gmail.com>
2023-12-24 17:35:20 -0300

    style: Adding new colors in index.css, setting font-family in tailwind.config and adding related file (postcss)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,22 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'licorice': '#210f04',
+      'seal-brown': '#582f0e',
+      'light-blue': '#9bbec7',
+      'salmon-pink': '#FFA5AB',
+      'mindaro': '#c8e087',
+      'jasmine': '#EECF6D',
+      'light-green': '#aceb98',
+      'tomato': '#f15946',
+    },  
+    backgroundImage: {
+      'canvas': "url('https://img.freepik.com/free-photo/white-paper-texture_1194-2324.jpg?w=996&t=st=1703445028~exp=1703445628~hmac=a0e3642cf9ec4f9eaf72608b26e506aa9a1ffa3fd0bbbe19713820eb90d6414d')",
+      'footer-texture': "url('/img/footer-texture.png')",
+    }
   },
   plugins: [],
 }
