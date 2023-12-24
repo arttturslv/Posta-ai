0ecaf42d653fa5d5281909984fd4c708b6bfd5eb
arttturslv <arttturslv@gmail.com>
2023-12-24 19:24:47 -0300

    feat: adding colors by random number

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,43 @@ import './index.css'
 import Card from './components/Card';
 
 function App() {
-
+  
+  const colors = {
+    lightblue: '#9bbec7',
+    salmonpink: '#FFA5AB',
+    mindaro: '#c8e087',
+    jasmine: '#EECF6D',
+    lightgreen: '#aceb98',
+    tomato: '#f15946',
+  };
+  
+  function changeColor() {
+    const colorNames = Object.keys(colors);
+    const randomIndex = Math.floor(Math.random() * colorNames.length);
+    const randomColorName = colorNames[randomIndex];
+    const randomColor = colors[randomColorName];
+    return randomColor;
+  }
+  
   return (
-    <div className="h-screen w-full max-w-[1600px] ">
-          <div className=" ">
+    <div className="h-screen w-full ">
+          <div className="flex flex-col gap-3 items-center">
 
         <div className='flex items-center h-[300px] justify-around flex-col '>
           <h1 className="text-[5vw] text-center text-[#ffff] font-bold">
-            Poste isso!
+            Posta aí!
           </h1>
           <div>
 
             <form className='flex gap-5 items-end'>
               <div>
                 <label htmlFor="mensagem" className="text-sm font-medium ">Mensagem</label>
-                <input type="search" className="disabled:bg-slate-50 invalid:border-pink-500 w-full p-4 ps-10 text-sm placeholder-seal-brown text-seal-brown font-medium border  rounded-lg bg-jasmine" placeholder="Eae galera, como vocês estão?" required></input>
+                <input type="search" className="disabled:bg-slate-50 invalid:border-pink-500 w-full p-4 ps-10 text-sm placeholder-[#6f6363] text-seal-brown font-medium border  rounded-lg bg-jasmine" placeholder="Eae galera, como vocês estão?" required></input>
               </div>
 
               <div>
                 <label htmlFor="mensagem" className="text-sm font-medium ">Autor</label>
-                <input type="search" className="w-full p-4 ps-10 text-sm placeholder-seal-brown text-seal-brown font-medium border rounded-lg bg-jasmine" placeholder="Artur" ></input>
+                <input type="search" className="w-full p-4 ps-10 text-sm placeholder-[#6f6363] text-seal-brown font-medium border rounded-lg bg-jasmine" placeholder="Artur" ></input>
               </div>
 
               <button type="button" className='h-[55px] px-4 rounded-lg text-sm text-seal-brown border-2 font-medium border-[#ffff] bg-jasmine hover:bg-[#553029] hover:text-[#fff]'>Colar</button>
@@ -29,15 +46,12 @@ function App() {
 
           </div>
         </div>
-        <div className='flex h-[500px] gap-4 flex-wrap'>
-          <Card note="Eae galera, como vocês estão?" author="Artur"></Card>
-          <Card note="Eae galera, como vocês estão?" author="Artur"></Card>
-          <Card note="Eae galera, como vocês estão?" author="Artur"></Card>
-          <Card note="Eae galera, como vocês estão?" author="Artur"></Card>
-          <Card note="Eae galera, como vocês estão?" author="Artur"></Card>
-          <Card note="Eae galera, como vocês estão?" author="Artur"></Card>
-          <Card note="Eae galera, como vocês estão?" author="Artur"></Card>
-
+        <div id='Panel' className='flex h-[500px] gap-6 flex-wrap max-w-[1300px] pt-10'>
+        <Card note="Eae galera, como vocês estão?" color={changeColor()}  author="Artur"></Card>
+        <Card note="FELIZ NATAL" color={changeColor()} author="Artur"></Card>
+        <Card note="TESTE21" color={changeColor()} author="Artur"></Card>
+        <Card note="#Brasilsil!" color={changeColor()} author="Artur"></Card>
+        <Card note="😁😀😀" color={changeColor()} author="Artur"></Card>
         </div>
     </div>
     </div>
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,10 @@
 
-export default function Card({note, author}) {
+export default function Card({note, author, color}) {
+
+
     return (
-    <div className="h-[200px] w-[200px] flex bg-jasmine p-2 pt-4">
+
+    <div className={'h-[200px] w-[200px] flex p-2 pt-4 '} style={{backgroundColor: color}}>
         <h2 className=" h-[80%] text-[#514] font-light ">{note}</h2>
         <h6 className="h-[20%] text-[#514] self-end pr-4 font-medium">{author}</h6>
     </div>
