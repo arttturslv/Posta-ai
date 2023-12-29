0162ce73c7b0da6ae58199fc727574ab3e1fd659
arttturslv <arttturslv@gmail.com>
2023-12-29 16:08:34 -0300

    style: improving button "colar" on mobile view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,34 +23,6 @@ function App() {
     return randomColor;
   };
 
-
-  // function onSend(e) {
-  //     e.preventDefault();
-  //     const panel = document.getElementById('Panel')
-      
-  //     if(message.value!=null) {
-
-  //     if(message.value.length > 0 && message.value.length<120) { //deu ruim
-  //       if(author.value.length > 0 && author.value.length<120) {
- 
-
-  //       postar(message.value, author.value);
-
-   
-
-  //       } else {
-  //         author.classList.add('pulse');
-  //       }
-  //    }else {
-  //     message.classList.add('pulse');
-
-  //    }
-
-  //   } else {
-  //     message.classList.add('pulse');
-  //   }
-  //   }
-
   const author = document.getElementById('Author');
   const message = document.getElementById('Message');
   
@@ -60,7 +32,7 @@ function App() {
     e.preventDefault();
     const note = message.value;
     
-    if(note==null || note.length<0 || note.length>120) {
+    if(note==null || note.length<1 || note.length>120) {
       message.classList.add('pulse');
     } else {
       postar(note, author.value);
@@ -82,8 +54,6 @@ function App() {
         } 
       }).then((response) => response.json())
       .then((json) => { 
-
-        console.log(json)
 
         setCards(json.data)
         setPostRenderizados(postRenderizados+json.skipValue);
@@ -119,40 +89,35 @@ function App() {
     }
 
   const [cards, setCards] = useState([
-    { note: "Eae galera, como vocês estão?", color: changeColor(), author: "Felipe" },
-    { note: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaa aaaaaaa aaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaa aaaaa aaaaaaa aaaaaaaaaa aaaaaa aaaa", color: changeColor(), author: "Artur" },
-    { note: "TESTE21", color: changeColor(), author: "Artur" },
-    { note: "#Brasilsil!", color: changeColor(), author: "Artur" },
-    { note: "😁😀😀", color: changeColor(), author: "Artur" },
   ]);
 
   return (
     <div className="h-screen w-full ">
-          <div className="flex flex-col gap-3 items-center">
+          <div className="flex flex-col gap-3 items-center ">
 
-        <div className='flex items-center h-[300px] justify-around flex-col '>
-          <h1 className="text-[5vw] text-center text-[#ffff] font-bold">
+        <div className='flex items-center h-[300px] justify-around flex-col px-3'>
+          <h1 className="sm:text-[5vw] text-[2rem] xl:text-[6rem] text-center text-[#ffff] font-bold pt-10 my-5">
             Posta aí!
           </h1>
           <div>
 
-            <form className='flex gap-5 items-end'>
+            <form className='block gap-5 items-end  sm:flex'>
               <div>
                 <label htmlFor="mensagem" className="text-sm font-medium ">Mensagem</label>
-                <input onChange={handleChange} id="Message" type="text" maxLength={120} className=" disabled:bg-slate-50 invalid:border-pink-500 w-full p-4 ps-10 text-sm placeholder-[#6f6363] text-seal-brown font-medium border  rounded-lg bg-jasmine" placeholder="Eae galera, como vocês estão?" required></input>
+                <input onChange={handleChange} id="Message" type="text" maxLength={120} className=" shadow-inner disabled:bg-slate-50 invalid:border-pink-500 w-full p-4 ps-10 text-sm placeholder-[#6f6363] text-seal-brown font-medium border  rounded-lg bg-jasmine" placeholder="Eae galera, como vocês estão?" required></input>
               </div>
 
               <div>
                 <label htmlFor="mensagem" className="text-sm font-medium ">Autor</label>
-                <input onChange={handleChange} id="Author" type="text" maxLength={14} className="w-full p-4 ps-10 text-sm placeholder-[#6f6363] text-seal-brown font-medium border rounded-lg bg-jasmine" placeholder="Artur" ></input>
+                <input onChange={handleChange} id="Author" type="text" maxLength={14} className="w-full shadow-inner p-4 ps-10 text-sm placeholder-[#6f6363] text-seal-brown font-medium border rounded-lg bg-jasmine" placeholder="Artur" ></input>
               </div>
 
-              <button type="submit" onClick={(e) => onSend(e)} className='h-[55px] px-4 rounded-lg text-sm text-seal-brown border-2 font-medium border-[#ffff] bg-jasmine hover:bg-[#553029] hover:text-[#fff]'>Colar</button>
+              <button type="submit" onClick={(e) => onSend(e)} className='mt-6 h-[55px] sm:w-auto w-[100%] self-center px-4 rounded-lg text-sm text-seal-brown border-2 font-medium border-[#ffff] bg-jasmine hover:bg-[#553029] hover:text-[#fff]'>Colar</button>
             </form>
 
           </div>
         </div>
-        <div id='Panel' className='flex h-[500px] gap-6 flex-wrap max-w-[1300px] pt-10 justify-center'>
+        <div id='Panel' className='flex h-[500px] gap-6 flex-wrap max-w-[1300px] pt-[5rem] justify-center'>
             {cards.map((card, index) => (
               <Card
                 key={index}
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({note, author, color}) {
 
     return (
 
-    <div className={'h-[200px] w-[200px] flex flex-col break-words  px-2 pt-4 '} style={{backgroundColor: color}}>
+    <div className={'h-[200px] w-[200px] flex flex-col break-words shadow-2xl px-2 pt-4 '} style={{backgroundColor: color}}>
         <h2 className=" h-[80%]  text-[#514] font-light text-wrap max-w-[100%]">{note}</h2>
         <h6 className="h-[20%] w-[100%] text-[#514] pr-4 text-right font-medium ">{author}</h6>
     </div>
